fb0afd3121f9cafe5b184cdd75662c270391bb70
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-04-02 09:44:43 -0700

    add boilerplate from cookiecutter

---
+++ tests/test_gitgoing.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_gitgoing
+----------------------------------
+
+Tests for `gitgoing` module.
+"""
+
+import unittest
+
+from gitgoing import gitgoing
+
+
+class TestGitgoing(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
