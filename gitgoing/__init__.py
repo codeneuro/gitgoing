5d378fbe950353f2fa0d57d4aa109e65d87adb6e
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-04-07 16:08:33 -0700

    change import gitgoing to be py3k compatible (maybe)

--- gitgoing/__init__.py
+++ gitgoing/__init__.py
@@ -4,5 +4,5 @@ __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@gmail.com'
 __version__ = '0.1.0'
 
-from gitgoing import mean_plus_one, std_plus_one, cv, is_neuron_component, \
+from .gitgoing import mean_plus_one, std_plus_one, cv, is_neuron_component, \
     NEURON_COMPONENTS
